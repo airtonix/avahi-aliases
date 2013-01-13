ab4495cac5f8cb883bce67ee7699eea7d614af1f
Sven <sven@5sw.de>
2013-01-13 13:35:21 +0100

    Remove commented-out code.

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-#IPADDRESS=`ifconfig | head -n2 | tail -n1 | awk '{print $2}' | awk -F : '{print $2}'`
-#HOSTNAME=`avahi-resolve-host-name -a  | awk '{ print $2 }'`
+
 SCRIPT_PATH=${0%/*}
 . $SCRIPT_PATH/lib/coloured_messages.sh
 
@@ -23,7 +22,6 @@ sudo mkdir /etc/avahi/aliases.d/
 
 message "Making central avahi-aliases file" "header"
 sudo touch /etc/avahi/aliases
-#message "Fill this with aliases (one per line) to ${HOSTNAME}" "result"
 
 message "Installation of Avahi-Aliases Done" "success"
 
